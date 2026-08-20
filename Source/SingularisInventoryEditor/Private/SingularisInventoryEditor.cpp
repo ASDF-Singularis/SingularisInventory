24a010a123006db4d1cb90a9618b68902a33ada4
TrifingZW <TrifingZW@gmail.com>
2026-08-20 22:08:27 +0800

    feat: 为口袋控件添加资产工厂与编辑器新建入口
    工厂生成 UWidgetBlueprint 资产，使口袋控件可在编辑器中以蓝图子类形式创建并编写展示逻辑。

--- Source/SingularisInventoryEditor/Private/SingularisInventoryEditor.cpp
+++ Source/SingularisInventoryEditor/Private/SingularisInventoryEditor.cpp
@@ -3,6 +3,7 @@
 #include <AssetToolsModule.h>
 
 #include "Factories/SingularisItemFactory.h"
+#include "Factories/SingularisPocketWidgetFactory.h"
 
 #define LOCTEXT_NAMESPACE "FSingularisInventoryEditorModule"
 
@@ -18,6 +19,11 @@ void FSingularisInventoryEditorModule::StartupModule()
 	RegisterAssetTypeAction(
 		AssetTools,
 		MakeShareable(new FAssetTypeActions_SingularisItem(SingularisPluginCategory))
+	);
+
+	RegisterAssetTypeAction(
+		AssetTools,
+		MakeShareable(new FAssetTypeActions_SingularisPocketWidget(SingularisPluginCategory))
 	);
 }
 
