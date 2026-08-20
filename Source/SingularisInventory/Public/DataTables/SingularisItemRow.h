5f903df7b47c560ef02924105591b9a44b6a3444
TrifingZW <TrifingZW@gmail.com>
2026-08-20 16:52:29 +0800

    feat: 为 FSingularisMagicalElementRow 添加属性字段

--- Source/SingularisInventory/Public/DataTables/SingularisItemRow.h
+++ Source/SingularisInventory/Public/DataTables/SingularisItemRow.h
@@ -4,8 +4,28 @@
 
 #include "SingularisItemRow.generated.h"
 
+class USingularisItem;
+
 USTRUCT(BlueprintType)
 struct FSingularisMagicalElementRow : public FTableRowBase
 {
 	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int Id = -1;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FText Name{};
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FText Description{};
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UTexture2D* Icon = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TSubclassOf<USingularisItem> ItemClass = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TSubclassOf<AActor> FormActorClass = nullptr;
 };
