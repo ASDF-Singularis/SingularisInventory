b136c4bfb89a57bd2cbbcb7447ed5670be095423
TrifingZW <TrifingZW@gmail.com>
2026-08-20 13:00:15 +0800

    feat: 添加 USingularisInventoryComponent 组件基类

---
+++ Source/SingularisInventory/Private/Components/SingularisInventoryComponent.cpp
@@ -0,0 +1,25 @@
+﻿#include "Components/SingularisInventoryComponent.h"
+
+USingularisInventoryComponent::USingularisInventoryComponent()
+{
+	SetIsReplicatedByDefault(true);
+
+	PrimaryComponentTick.bStartWithTickEnabled = false;
+	PrimaryComponentTick.bCanEverTick = false;
+
+	bAutoActivate = true;
+}
+
+void USingularisInventoryComponent::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void USingularisInventoryComponent::TickComponent(
+	const float DeltaTime,
+	const ELevelTick TickType,
+	FActorComponentTickFunction* ThisTickFunction
+)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+}
---
+++ Source/SingularisInventory/Public/Components/SingularisInventoryComponent.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+
+#include <CoreMinimal.h>
+#include <Components/ActorComponent.h>
+
+#include "SingularisInventoryComponent.generated.h"
+
+UCLASS(
+	Blueprintable,
+	BlueprintType,
+	ClassGroup = ("Singularis"),
+	meta = (BlueprintSpawnableComponent, DisplayName = "引力奇点物库存组件")
+)
+class SINGULARISINVENTORY_API USingularisInventoryComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+#pragma region Constructors
+
+	USingularisInventoryComponent();
+
+#pragma endregion
+
+#pragma region ActorComponent Interface
+
+	virtual void BeginPlay() override;
+	virtual void TickComponent(
+		float DeltaTime,
+		ELevelTick TickType,
+		FActorComponentTickFunction* ThisTickFunction
+	) override;
+
+#pragma endregion
+};
