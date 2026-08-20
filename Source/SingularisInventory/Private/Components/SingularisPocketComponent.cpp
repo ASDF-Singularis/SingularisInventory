19d9318b1aa42a44946b0e143dc694ce9d04f098
TrifingZW <TrifingZW@gmail.com>
2026-08-20 12:58:42 +0800

    feat: 添加 USingularisPocketComponent 组件基类

---
+++ Source/SingularisInventory/Private/Components/SingularisPocketComponent.cpp
@@ -0,0 +1,25 @@
+﻿#include "Components/SingularisPocketComponent.h"
+
+USingularisPocketComponent::USingularisPocketComponent()
+{
+	SetIsReplicatedByDefault(true);
+
+	PrimaryComponentTick.bStartWithTickEnabled = false;
+	PrimaryComponentTick.bCanEverTick = false;
+
+	bAutoActivate = true;
+}
+
+void USingularisPocketComponent::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void USingularisPocketComponent::TickComponent(
+	const float DeltaTime,
+	const ELevelTick TickType,
+	FActorComponentTickFunction* ThisTickFunction
+)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+}
---
+++ Source/SingularisInventory/Public/Components/SingularisPocketComponent.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+
+#include <CoreMinimal.h>
+#include <Components/ActorComponent.h>
+
+#include "SingularisPocketComponent.generated.h"
+
+UCLASS(
+	Blueprintable,
+	BlueprintType,
+	ClassGroup = ("Singularis"),
+	meta = (BlueprintSpawnableComponent, DisplayName = "引力奇点口袋组件")
+)
+class SINGULARISINVENTORY_API USingularisPocketComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+#pragma region Constructors
+
+	USingularisPocketComponent();
+
+#pragma endregion
+
+#pragma region ActorComponent Interface
+
+	virtual void BeginPlay() override;
+	virtual void TickComponent(
+		float DeltaTime,
+		ELevelTick TickType,
+		FActorComponentTickFunction* ThisTickFunction
+	) override;
+
+#pragma endregion
+};
