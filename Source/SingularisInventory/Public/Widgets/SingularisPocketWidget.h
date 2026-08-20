33293adaaf2695e63f589e3b73fc328fbe61e601
TrifingZW <TrifingZW@gmail.com>
2026-08-20 12:53:13 +0800

    feat: 添加 USingularisPocketWidget 控件基类

---
+++ Source/SingularisInventory/Public/Widgets/SingularisPocketWidget.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+#include <CoreMinimal.h>
+#include <Blueprint/UserWidget.h>
+
+#include "SingularisPocketWidget.generated.h"
+
+/**
+ * 引力奇点口袋控件
+ */
+UCLASS(Abstract, Blueprintable)
+class SINGULARISINVENTORY_API USingularisPocketWidget : public UUserWidget
+{
+	GENERATED_BODY()
+};
