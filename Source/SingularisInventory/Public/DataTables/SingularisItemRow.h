55de882b0855f5d2492dd36f5be4c5d00081faa1
TrifingZW <TrifingZW@gmail.com>
2026-08-20 14:25:43 +0800

    feat: 添加 FSingularisMagicalElementRow 数据表行结构

---
+++ Source/SingularisInventory/Public/DataTables/SingularisItemRow.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+#include <CoreMinimal.h>
+
+#include "SingularisItemRow.generated.h"
+
+USTRUCT(BlueprintType)
+struct FSingularisMagicalElementRow : public FTableRowBase
+{
+	GENERATED_BODY()
+};
